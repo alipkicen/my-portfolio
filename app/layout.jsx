6a56443e940419dd3de5f1db37f185c2cfca66bd
Krishan Arpidani <alip.kicen@gmail.com>
2025-09-06 20:29:16 +0800

    adding analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 //components
 import Header from "@/components/Header";
